5fdc76e1df1b0f25abe4c1cfbc73876782b4194d
gissandrogama <gissandrogama@gmail.com>
2021-02-10 14:02:12 -0300

    configuraçao do sobelow

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule EventsApi.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 
