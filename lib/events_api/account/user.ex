f08435e2779711e56ad48fc255f5a174bce38ed1
gissandrogama <gissandrogama@gmail.com>
2021-02-10 20:08:19 -0300

    criaçao de schemas de users e events

---
+++ lib/events_api/account/user.ex
@@ -0,0 +1,24 @@
+defmodule EventsApi.Account.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias EventsApi.Doings.Event
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password_hash, :string
+    has_many :events, Event
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :password_hash])
+    |> validate_required([:name, :email, :password_hash])
+  end
+end
---
+++ lib/events_api/doings/event.ex
@@ -0,0 +1,30 @@
+defmodule EventsApi.Doings.Event do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias EventsApi.Account.User
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "events" do
+    field :andress, :string
+    field :date_finish, :string
+    field :date_initial, :string
+    field :description, :string
+    field :end_time, :string
+    field :place, :string
+    field :start_time, :string
+    field :title, :string
+    field :url_img, :string
+    belongs_to :user, User
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(event, attrs) do
+    event
+    |> cast(attrs, [:title, :date_initial, :date_finish, :description, :start_time, :end_time, :place, :andress, :url_img, :user_id])
+    |> foreign_key_constraint(:user_id)
+    |> validate_required([:title, :date_initial, :date_finish, :description, :start_time, :end_time, :place, :andress, :url_img, :user_id])
+  end
+end
